8987eb68960f8eb85503e49caac24b6bbf558768
NarimanSh1 <143985446+NarimanSh1@users.noreply.github.com>
2023-11-20 22:43:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,3 @@ function toggleSection(sectionId) {
     mathSection.style.display = (sectionId === 'math') ? 'block' : 'none';
     chemistrySection.style.display = (sectionId === 'chemistry') ? 'block' : 'none';
 }
-
-function toggleDetails(element) {
-    var details = element.getElementsByClassName('problem-details')[0];
-    details.style.display = details.style.display === 'block' ? 'none' : 'block';
-}
